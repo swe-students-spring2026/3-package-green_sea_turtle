b4bad0dd6cfae604ba3eff3a9f5daba82d475bce
LukeySan <lukegogreenly@gmail.com>
2026-03-29 22:58:48 -0400

    feat: add exclude non matching recipe test and empty ingredients test

--- tests/test_matcher.py
+++ tests/test_matcher.py
@@ -12,7 +12,11 @@ class Tests:
         actual = True
         assert actual == expected, "Expected True to be equal to True!"
 
-    def test_matches_kale_salad(self, example_fixture):
+    def test_empty_ingredients_list(self):
+        results = match_recipes([])
+        assert len(results) == 0, "Expected an empty list of results!"
+
+    def test_matches_kale_salad(self):
         ingredients = ["leeks", "beet", "olive oil", "salt", "pepper", 
                    "lentils", "vegetable stock", "kale", "tahini", 
                    "lemon", "maple syrup"]
@@ -21,3 +25,13 @@ class Tests:
         recipe_names = [recipe["name"] for recipe in results]
         
         assert "Kale, Lentil & Roasted Beet Salad" in recipe_names, "Expected Kale, Lentil & Roasted Beet Salad to be in the results!"
+    
+    def test_excludes_non_matching_recipes(self):
+        ingredients = ["leeks", "beet", "olive oil", "salt", "pepper", 
+                   "lentils", "vegetable stock", "kale", "tahini", 
+                   "lemon"]
+        results = match_recipes(ingredients)
+
+        recipe_names = [recipe["name"] for recipe in results]
+
+        assert "Kale, Lentil & Roasted Beet Salad" not in recipe_names, "Expected Kale, Lentil & Roasted Beet Salad to not be in the results!"
